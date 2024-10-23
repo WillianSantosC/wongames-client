5f0bc4c06e4762cf4acc1bbaf9584d16a457d9c1
Willian Santos <williansantosd2@gmail.com>
2024-10-23 16:29:25 -0300

    feat: added global styles to button elements

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -13,6 +13,15 @@ const globalCss = defineGlobalStyles({
     fontSize: "62.5%",
   },
 
+  button: {
+    cursor: "pointer",
+    "&:active": {
+      transform: "translateY(1px)",
+      textShadow: "-1px -1px 0px #777, -1px -1px 0px #777",
+      boxShadow: "inset 1px 1px 4px #222",
+    },
+  },
+
   "html, body": {
     height: "100%",
   },
