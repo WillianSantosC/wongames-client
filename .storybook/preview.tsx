e12225ef6284bf95fc8b8c88fccc2228200a811f
Willian Santos <williansantosd2@gmail.com>
2024-10-31 09:42:02 -0300

    feat: added custom backgrounds to storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,5 +1,16 @@
 import React from "react";
 import "../src/app/globals.css";
+import { token } from "../styled-system/tokens";
+
+export const parameters = {
+  backgrounds: {
+    values: [
+      { name: "won-light", value: token("colors.white") },
+      { name: "won-dark", value: token("colors.mainBg") },
+    ],
+    default: "won-light",
+  },
+};
 
 export const decorators = [
   (Story) => (
