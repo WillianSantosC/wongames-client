91ded6e522345647b854578483d711f2a04b54d1
Willian Santos <williansantosd2@gmail.com>
2024-10-31 18:50:44 -0300

    feat: changed scrollbar style

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -35,6 +35,49 @@ const globalCss = defineGlobalStyles({
       "Poppins, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
     backgroundColor: "token(colors.mainBg)",
   },
+
+  // ScrollBar Style
+  "::-webkit-scrollbar": {
+    width: "7px",
+    height: "7px",
+  },
+
+  "::-webkit-scrollbar-button": {
+    width: "0px",
+    height: "0px",
+  },
+
+  "::-webkit-scrollbar-thumb": {
+    background: "#f231a5",
+    border: "0px none #d21414",
+    borderRadius: "6px",
+  },
+
+  "::-webkit-scrollbar-thumb:hover": {
+    background: "#f231a5",
+  },
+
+  "::-webkit-scrollbar-thumb:active": {
+    background: "#f231a5",
+  },
+
+  "::-webkit-scrollbar-track": {
+    background: "#666666",
+    border: "9px solid #06092b",
+    borderRadius: "53px",
+  },
+
+  "::-webkit-scrollbar-track:hover": {
+    background: "#666666",
+  },
+
+  "::-webkit-scrollbar-track:active": {
+    background: "#f231a5",
+  },
+
+  "::-webkit-scrollbar-corner": {
+    background: "transparent",
+  },
 });
 
 export default globalCss;
