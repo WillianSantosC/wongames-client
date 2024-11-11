da994df060478254fabfe5594734dd0fd713f294
Willian Santos <williansantosd2@gmail.com>
2024-11-11 16:09:53 -0300

    feat: added styles to Empty component

--- src/components/Empty/styles.tsx
+++ src/components/Empty/styles.tsx
@@ -1,17 +1,32 @@
 import { styled } from "@/panda/jsx";
 
 export const Wrapper = styled("main", {
-  base: {},
+  base: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    textAlign: "center",
+  },
 });
 
 export const Image = styled("img", {
-  base: {},
+  base: {
+    maxWidth: "100%",
+  },
 });
 
 export const Title = styled("h2", {
-  base: {},
+  base: {
+    color: "token(colors.primary)",
+    fontSize: "token(font.sizes.xxlarge)",
+  },
 });
 
 export const Description = styled("p", {
-  base: {},
+  base: {
+    color: "token(colors.white)",
+    fontSize: "token(font.sizes.large)",
+    fontWeight: "token(font.light)",
+    marginBottom: "token(spacings.medium)",
+  },
 });
